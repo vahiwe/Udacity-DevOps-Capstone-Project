87e0de077ffb72a3254cabb2ef1924a87ea611b8
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-09-28 12:49:55 +0100

    Update Configuration files

---
+++ initial-setup.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Update Installed Packages
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Install Some Necessary Packages
+sudo apt-get -y install python3.6 default-jdk python3-venv unzip make tidy pylint
+
+# Install and Setup Docker
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo chmod 666 /var/run/docker.sock
+
+# Install Jenkins and start it
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+
+sudo apt-get -y install jenkins
+
+sudo systemctl start jenkins
+
+systemctl status jenkins
+
+sudo systemctl enable jenkins
+
+# Install AWS CLI V2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+# Install Kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Install hadolint
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64 && sudo chmod +x /bin/hadolint
+
